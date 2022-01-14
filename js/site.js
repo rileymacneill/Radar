d6f8a15a92174226a4b949adbe1acffcf22364cb
rileymacneill <41310367+rileymacneill@users.noreply.github.com>
2022-01-13 22:58:02 -0800

    My JS model

--- js/site.js
+++ js/site.js
@@ -1,65 +1,31 @@
-//get the values from the UI
-//starts our controller function
-function getValues() {
-    //get values from page
-    let startValue = document.getElementById("startValue").value;
-    let endValue = document.getElementById("endValue").value;
+function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let specialString = userString.replace(/[^a-zA-Z ]/g, "");
+    let lowerString = specialString.toLowerCase();
+    let revString = reverseString(lowerString);
+    displayString(revString,lowerString);
+}
 
-    //parse into Integers
-    startValue = parseInt(startValue);
-    endValue = parseInt(endValue);
+//Reverse the string
+function reverseString(lowerString) {
+    let revString = [];
+    //Reverse the string using a for loop
+    for (let index = lowerString.length - 1; index >= 0; index--) {
+        revString += lowerString[index];
+    }
+    return revString;
+}
 
-    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
-
-        //call generate numbers
-        numbers = generateNumbers(startValue, endValue);
-
-        //call display numbers
-        displayNumbers(numbers);
-
+//View function
+function displayString(revString,lowerString) {
+    if (revString == lowerString) {
+        document.getElementById("msg").innerHTML = `Your string is Palindrome: ${revString}`;
+        document.getElementById("alert").classList.remove("invisible");
+        document.getElementById("alert").classList.add("alert-success");
     } else {
-        alert("You must enter integers");
+        document.getElementById("msg").innerHTML = `Your string is NOT a Palindrome: ${revString}`;
+        document.getElementById("alert").classList.remove("invisible");
+        document.getElementById("alert").classList.add("alert-danger");
     }
-}
-
-//generate numbers from the start value to the end value
-//logic function(s)
-function generateNumbers(sValue, eValue) {
-
-    let numbers = [];
-
-    //we want to get all numbers from start to end
-    for (let index = sValue; index <= eValue; index++) {
-
-        //this will execute in a loop until index = eValue
-        numbers.push(index);
-    }
-
-    return numbers;
-
-}
-
-//display numbers and mark even numbers bold
-//display or view functions
-function displayNumbers(numbers) {
-
-    let templateRows = "";
-
-    for (let index = 0; index < numbers.length; index++) {
-
-        let className = "even";
-        let number = numbers[index];
-
-        if (number % 2 == 0) {
-            className = "even";
-        } else {
-            className = "odd";
-        }
-        
-        //This does render correctly with Prism see the source
-        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
-    }
-
-    document.getElementById("results").innerHTML = templateRows;
-
 }
